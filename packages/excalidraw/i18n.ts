966d2f631cb95b8661ff56fc0056f9ae69a1a45c
nguyendinhquocx <quocnguyent10v@gmail.com>
2026-06-11 11:38:11 +0700

    Set Vietnamese as default language

--- packages/excalidraw/i18n.ts
+++ packages/excalidraw/i18n.ts
@@ -16,7 +16,7 @@ export interface Language {
 
 export type TranslationKeys = NestedKeyOf<typeof fallbackLangData>;
 
-export const defaultLang = { code: "en", label: "English" };
+export const defaultLang = { code: "vi-VN", label: "Tiếng Việt" };
 
 export const languages: Language[] = [
   defaultLang,
