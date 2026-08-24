720c1e88631ef426b8e4fb85c634c4876ebf7092
nguyendinhquocx <quocnguyent10v@gmail.com>
2026-08-24 19:50:20 +0700

    Polish favicon: reduce pencil size and add cache bust

---
+++ assets/channel-BVU4-BvI.js
@@ -0,0 +1,2 @@
+import{al as o,am as n}from"./index-BscW8BXT.js";const l=(a,r)=>o.lang.round(n.parse(a)[r]);export{l as c};
+//# sourceMappingURL=channel-BVU4-BvI.js.map
---
+++ assets/clone-Cjn6fREX.js
@@ -0,0 +1,2 @@
+import{a as r}from"./graph-Cgqg-UT8.js";var a=4;function n(o){return r(o,a)}export{n as c};
+//# sourceMappingURL=clone-Cjn6fREX.js.map
